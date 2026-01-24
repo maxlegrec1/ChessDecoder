3474cedf6cfad418556cf4141ad313eeb703c249
Maxime Vanderbeken <Maxtinos2508@gmail.com>
2026-01-24 21:38:40 +0000

    add git sync pgns

--- tests/test_evaluate_elo.py
+++ tests/test_evaluate_elo.py
@@ -5,7 +5,7 @@ import torch
 
 
 model = ChessDecoder(vocab_size=vocab_size)
-model.load_state_dict(torch.load("src/train/checkpoints/checkpoint_epoch_3.pt"))
+model.load_state_dict(torch.load("./checkpoints/checkpoint_epoch_3.pt"))
 # model.load_state_dict(torch.load("checkpoints/checkpoint_epoch_4.pt"))
 model.eval()
 
